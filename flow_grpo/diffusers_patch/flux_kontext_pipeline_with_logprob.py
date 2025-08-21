fix the precision in the pipeline

flow_grpo/diffusers_patch/flux_kontext_pipeline_with_logprob.py
@@ -166,6 +166,7 @@ def pipeline_with_logprob(
         generator,
         latents,
     )
+    latents = latents.float()
     if image_ids is not None:
         latent_ids = torch.cat([latent_ids, image_ids], dim=0)  # dim 0 is sequence dimension
     sigmas = np.linspace(1.0, 1 / num_inference_steps, num_inference_steps) if sigmas is None else sigmas
@@ -226,7 +227,7 @@ def pipeline_with_logprob(
                 print("log_prob is nan")
             noise_pred = noise_pred[:, : latents.size(1)]
 
-            noise_pred = noise_pred.to(prompt_embeds.dtype)
+            # noise_pred = noise_pred.to(prompt_embeds.dtype)
             latents_dtype = latents.dtype
 
             latents, log_prob, prev_latents_mean, std_dev_t = sde_step_with_logprob(
@@ -237,8 +238,8 @@ def pipeline_with_logprob(
                 noise_level=noise_level,
             )
             
-            if latents.dtype != latents_dtype:
-                latents = latents.to(latents_dtype)
+            # if latents.dtype != latents_dtype:
+            #     latents = latents.to(latents_dtype)
             all_latents.append(latents)
             all_log_probs.append(log_prob)
             # call the callback, if provided