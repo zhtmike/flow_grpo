40e1e38ae210fe8b3266bdbcaa8103af7b144fe7
liujie <jieliu@link.cuhk.edu.hk>
2025-09-11 15:24:35 +0800

    update qwen edit config

--- config/grpo.py
+++ config/grpo.py
@@ -639,7 +639,7 @@ def counting_qwenimage_edit():
     config.activation_checkpointing = True
     config.fsdp_optimizer_offload = True
     config.save_freq = 60 # epoch
-    config.eval_freq = 15
+    config.eval_freq = 30
     config.save_dir = 'logs/pickscore/qwenimage_edit'
     config.reward_fn = {
         "image_similarity": 0.2,
@@ -648,6 +648,47 @@ def counting_qwenimage_edit():
     config.per_prompt_stat_tracking = True
     return config
 
+def counting_qwenimage_edit_fast():
+    gpu_number=32
+    config = compressibility()
+    config.dataset = os.path.join(os.getcwd(), "dataset/counting_edit")
+
+    # flux
+    config.pretrained.model = "Qwen/Qwen-Image-Edit"
+    config.sample.num_steps = 10
+    config.sample.eval_num_steps = 50
+    config.sample.guidance_scale = 4
+
+    config.resolution = 512
+    config.sample.train_batch_size = 4
+    config.sample.num_image_per_prompt = 16
+    config.sample.num_batches_per_epoch = int(32/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt))
+    assert config.sample.num_batches_per_epoch % 2 == 0, "Please set config.sample.num_batches_per_epoch to an even number! This ensures that config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch / 2, so that gradients are updated twice per epoch."
+    config.sample.test_batch_size = 4 # This bs is a special design, the test set has a total of 2048, to make gpu_num*bs*n as close as possible to 2048, because when the number of samples cannot be divided evenly by the number of cards, multi-card will fill the last batch to ensure each card has the same number of samples, affecting gradient synchronization.
+
+    config.train.batch_size = config.sample.train_batch_size
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch//2
+    config.train.num_inner_epochs = 1
+    config.train.beta = 0
+    config.sample.global_std = True
+    config.sample.same_latent = False
+    config.train.ema = False
+    config.sample.noise_level = 1.5
+    config.sample.sde_window_size = 3
+    config.sample.sde_window_range = (0, config.sample.num_steps//2)
+    config.mixed_precision = "bf16"
+    config.use_lora = True
+    config.activation_checkpointing = True
+    config.fsdp_optimizer_offload = True
+    config.save_freq = 60 # epoch
+    config.eval_freq = 30
+    config.save_dir = 'logs/pickscore/qwenimage_edit'
+    config.reward_fn = {
+        "image_similarity": 0.2,
+        "geneval": 0.8,
+    }
+    config.per_prompt_stat_tracking = True
+    return config
 
 def counting_qwenimage_edit_8gpu():
     gpu_number=8
@@ -674,19 +715,19 @@ def counting_qwenimage_edit_8gpu():
     config.sample.global_std = True
     config.sample.same_latent = False
     config.train.ema = False
-    config.sample.noise_level = 1.8
-    config.sample.sde_window_size = 2
-    config.sample.sde_window_range = (0, config.sample.num_steps//2)
+    config.sample.noise_level = 1.0
+    config.sample.sde_window_size = 0
+    # config.sample.sde_window_range = (0, config.sample.num_steps//2)
     config.mixed_precision = "bf16"
     config.use_lora = True
     config.activation_checkpointing = True
     config.fsdp_optimizer_offload = True
     config.save_freq = 60 # epoch
-    config.eval_freq = 15
+    config.eval_freq = 30
     config.save_dir = 'logs/pickscore/qwenimage_edit'
     config.reward_fn = {
-        # "image_similarity": 0.5,
-        "geneval": 1.0,
+        "image_similarity": 0.2,
+        "geneval": 0.8,
     }
     config.per_prompt_stat_tracking = True
     return config
